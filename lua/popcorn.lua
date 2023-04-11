a365f43517579b65bd147da28bb256a6cb3149fa
Javier Orfo <javierorfo@protonmail.com>
2023-04-11 12:21:05 -0300

    Update popcorn.lua

--- lua/popcorn.lua
+++ lua/popcorn.lua
@@ -1,8 +1,3 @@
--- #########################################################
--- # Maintainer: Mr. Charkuils                             #
--- # URL:        https://github.com/charkuils/nvim-popcorn #
--- #########################################################
-
 local borders = require'popcorn.util'.borders1
 local M = {}
 
