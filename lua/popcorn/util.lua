1abc015fe0b189d300f30177a358271ba0a4de3f
Javier Orfo <javierorfo@protonmail.com>
2023-04-11 12:21:17 -0300

    Update util.lua

--- lua/popcorn/util.lua
+++ lua/popcorn/util.lua
@@ -1,8 +1,3 @@
--- #########################################################
--- # Maintainer: Mr. Charkuils                             #
--- # URL:        https://github.com/charkuils/nvim-popcorn #
--- #########################################################
-
 local M = {}
 
 M.borders = {
