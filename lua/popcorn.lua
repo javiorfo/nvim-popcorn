7ddba6c922ca4c8606b813119d655b1b86c96830
Javier Orfo <javierorfo@prontonmail.com>
2024-01-10 11:07:26 -0300

    bug fixed in ESC keymap

--- lua/popcorn.lua
+++ lua/popcorn.lua
@@ -149,8 +149,8 @@ function popcorn:pop()
         })
 
         local map_opts = { noremap = true, silent = true }
-        vim.api.nvim_buf_set_keymap(buf_text, 'n', '<esc>', '<cmd>quit<cr>', map_opts)
-        vim.api.nvim_buf_set_keymap(buf_text, 'n', '<cr>', '<cmd>lua require("popcorn").execute_callback()<cr>', map_opts)
+        vim.api.nvim_buf_set_keymap(0, 'n', '<esc>', '<cmd>quit<cr>', map_opts)
+        vim.api.nvim_buf_set_keymap(0, 'n', '<cr>', '<cmd>lua require("popcorn").execute_callback()<cr>', map_opts)
 
         if self.do_after then self.do_after() end
 
